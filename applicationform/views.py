2075fffd51aac15df1ca7593de8ec777c5cb1b9f
Parth Vaish <parthvaish96@gmail.com>
2019-06-14 00:52:10 +0530

    hello world made

--- applicationform/views.py
+++ applicationform/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def exam(request):
+    return HttpResponse("hello world")
