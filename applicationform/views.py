ad5fcbf4fb4460e7db96f1fe14a129f99371de3e
Parth Vaish <parthvaish96@gmail.com>
2019-06-16 13:25:55 +0530

    frontend copied

--- applicationform/views.py
+++ applicationform/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'buy.html')
 
 def exam(request):
     return HttpResponse("hello world")
