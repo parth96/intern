ca856eda3d122d49dc8a9da6e17dc388cd0c18f2
Parth Vaish <parthvaish96@gmail.com>
2019-06-19 21:35:11 +0530

    database almost done

--- applicationform/views.py
+++ applicationform/views.py
@@ -1,58 +1,85 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
+from applicationform.models import applicationform
+# from django.db import IntegrityError
 # Create your views here.
 from django.template import RequestContext
 
 
 def index(request):
     if request.method == "POST":
-        name = request.POST['name']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        fname = request.POST['fname']
-        foccu = request.POST['foccu']
-        bday = request.POST['bday']
-        gender = request.POST['gender']
-        desig = request.POST['desig']
-        cpf = request.POST['cpf']
-        section = request.POST['section']
-        city = request.POST['city']
-        phno = request.POST['phno']
-        mono = request.POST['mono']
-        insname = request.POST['insname']
-        brname = request.POST['brname']
-        sem = request.POST['sem']
-        semmarks = request.POST['semmarks']
-        address = request.POST['address']
-        menname = request.POST['menname']
-        des1 = request.POST['des1']
-        site = request.POST['site']
-        loc = request.POST['loc']
+         instance = applicationform.objects.create(name = request.POST['name'],email = request.POST['email'],phone = request.POST['phone'],fname = request.POST['fname'],foccu = request.POST['foccu'],bday = request.POST['bday'],gender = request.POST['gender'],desig = request.POST['desig'],cpf = request.POST['cpf'],section = request.POST['section'],city = request.POST['city'],phno = request.POST['phno'],mono = request.POST['mono'],insname = request.POST['insname'],sem = request.POST['sem'],semmarks = request.POST['semmarks'],address = request.POST['address'],menname = request.POST['menname'],des1 = request.POST['des1'],site = request.POST['site'],loc = request.POST['loc'])
+         instance.save()
+                                                  # email=request.POST.get('email'),
+                                                  # fname=request.POST.get('fname'),
+                                                  # foccu=request.POST.get('foccu'),
+                                                  # phone=request.POST.get('phone'),
+                                                  # # bday=request.POST.get('bday'),
+                                                  # gender=request.POST.get('gender'),
+                                                  # desig=request.POST.get('desig'),
+                                                  # cpf=request.POST.get('cpf'),
+                                                  # section=request.POST.get('section'),
+                                                  # city=request.POST.get('city'),
+                                                  # phno=request.POST.get('phno'),
+                                                  # # mono=request.POST.get('mono'),
+                                                  # insname=request.POST.get('insname'),
+                                                  # brname=request.POST.get('brname'),
+                                                  # sem=request.POST.get('sem'),
+                                                  # semmarks=request.POST.get('semmarks'),
+                                                  # address=request.POST.get('address'),
+                                                  # menname=request.POST.get('menname'),
+                                                  # des1=request.POST.get('des1'),
+                                                  # site=request.POST.get('site'),
+                                                  # loc=request.POST.get('loc'))
 
-        print(name)
-        print(email)
-        print(phone)
-        print(fname)
-        print(foccu)
-        print(bday)
-        print(gender)
-        print(desig)
-        print(cpf)
-        print(section)
-        print(city)
-        print(phno)
-        print(mono)
-        print(insname)
-        print(brname)
-        print(sem)
-        print(semmarks)
-        print(address)
-        print(menname)
-        print(des1)
-        print(site)
-        print(loc)
+
+    # if request.method == "POST":
+    #     name = request.POST['name']
+    #     email = request.POST['email']
+    #     phone = request.POST['phone']
+    #     fname = request.POST['fname']
+    #     foccu = request.POST['foccu']
+    #     bday = request.POST['bday']
+    #     gender = request.POST['gender']
+    #     desig = request.POST['desig']
+    #     cpf = request.POST['cpf']
+    #     section = request.POST['section']
+    #     city = request.POST['city']
+    #     phno = request.POST['phno']
+    #     mono = request.POST['mono']
+    #     insname = request.POST['insname']
+    #     brname = request.POST['brname']
+    #     sem = request.POST['sem']
+    #     semmarks = request.POST['semmarks']
+    #     address = request.POST['address']
+    #     menname = request.POST['menname']
+    #     des1 = request.POST['des1']
+    #     site = request.POST['site']
+    #     loc = request.POST['loc']
+    #
+    #     print(name)
+    #     print(email)
+    #     print(phone)
+    #     print(fname)
+    #     print(foccu)
+    #     print(bday)
+    #     print(gender)
+    #     print(desig)
+    #     print(cpf)
+    #     print(section)
+    #     print(city)
+    #     print(phno)
+    #     print(mono)
+    #     print(insname)
+    #     print(brname)
+    #     print(sem)
+    #     print(semmarks)
+    #     print(address)
+    #     print(menname)
+    #     print(des1)
+    #     print(site)
+    #     print(loc)
 
     branch = {'MBA(HR)': 1, 'MBA(Finance)': 2, 'Geology': 3, 'Geophysics': 4, 'MSc. - Chemistry': 5,
               'Mech.Engineering': 6,
